d875a2f99fe50f091880d45aa223fd458ec10f99
xenophobe0824 <2201212077@stu.pku.edu.cn>
2022-11-16 20:24:38 +0800

    upload for github repo

--- docs/oxygen-webhelp/resources/skins/desktop/toc_driver.js
+++ docs/oxygen-webhelp/resources/skins/desktop/toc_driver.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
  */
 function loadSearchResources() {
     if (typeof window.indexerLanguage == 'undefined') {
-        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20221116051831", "oxygen-webhelp/search/index-1.js?uniqueId=20221116051831", "oxygen-webhelp/search/index-2.js?uniqueId=20221116051831", "oxygen-webhelp/search/index-3.js?uniqueId=20221116051831"];
+        var scripts = ["oxygen-webhelp/search/htmlFileInfoList.js?uniqueId=20221116081805", "oxygen-webhelp/search/index-1.js?uniqueId=20221116081805", "oxygen-webhelp/search/index-2.js?uniqueId=20221116081805", "oxygen-webhelp/search/index-3.js?uniqueId=20221116081805"];
         for (var entry in scripts) {
             var scriptTag = document.createElement("script");
             scriptTag.type = "text/javascript";
